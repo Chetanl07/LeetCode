41f8dbba53acd7d9917e58f4bda16f6b67559897
chetan07 <cnlavange03@gmail.com>
2026-05-11 21:41:37 +0530

    MultiplyString

---
+++ LeetCode/_43MultiplyString.java
@@ -0,0 +1,13 @@
+public class _43MultiplyString
+{
+    public static void main(String[] args) {
+        
+       String num1="123456789";
+       String num2="987654321";
+
+       int a= Integer.parseInt(num1);
+       int b= Integer.parseInt(num2);
+       String c = ""+(a*b);
+       System.out.println(c);
+    }
+}

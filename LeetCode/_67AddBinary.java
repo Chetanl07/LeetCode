89d570a979e3a6ad00f9bff7cf0b28fa2207d6df
chetan07 <cnlavange03@gmail.com>
2026-06-03 23:25:41 +0530

    Addition of binary numbers

---
+++ LeetCode/_67AddBinary.java
@@ -0,0 +1,35 @@
+public class _67AddBinary {
+    public static void main(String[] args) {
+        String a="11",b="1";
+
+        Solution67 s= new Solution67();
+        System.out.println(s.addBinary(a, b));
+        
+    }
+    
+}
+class Solution67 {
+    public String addBinary(String a, String b) 
+    {
+        StringBuilder s= new StringBuilder();
+        int i=a.length()-1;
+        int j=b.length()-1;
+
+        int carry=0;
+
+        while(i>=0 || j>=0 || carry>0)
+        {
+            int sum=carry;
+
+            if(i>=0) sum+=a.charAt(i--)-'0';
+
+            if(j>=0) sum+=b.charAt(j--)-'0';
+
+            s.append(sum%2);
+            carry=sum/2;
+
+        }
+        
+        return s.reverse().toString();
+    }
+}

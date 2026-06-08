bb2209f067fb6650bf0742808caecdaf922025c9
chetan07 <cnlavange03@gmail.com>
2026-06-08 23:26:10 +0530

    best time of sell and buy the stock

---
+++ LeetCode/_122BestTimeTOBuyAndSellStock2.java
@@ -0,0 +1,31 @@
+public class _122BestTimeTOBuyAndSellStock2 {
+    public static void main(String[] args) {
+        
+        Solution121 s= new Solution121();
+        int[] nums={7,1,5,3,6,4};
+
+        System.out.println(s.maxProfit(nums));
+    }
+    
+}
+class Solution122 {
+    public int maxProfit(int[] prices) {
+        
+     int sum=0;
+    int left=0;
+    int right=1;
+
+    while(left<prices.length)
+    {
+      if(prices[left]<prices[right]) 
+      {
+        sum+=prices[right]-prices[left];
+      }
+
+      left++;
+      right++;
+    }
+
+    return sum;
+    }
+}

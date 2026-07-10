fc1354f645a755e5abe79719ef6b5a27589a8909
chetan07 <cnlavange03@gmail.com>
2026-07-10 13:21:37 +0530

    Number of Students are in the query time

---
+++ LeetCode/_1450NumberOfStudents.java
@@ -0,0 +1,21 @@
+public class _1450NumberOfStudents {
+    public static void main(String[] args) {
+        Solution1450 sol=new Solution1450();
+        int[] startTime={1,2,3};
+        int[] endTime={3,2,7};
+        int queryTime=4;
+        System.out.println(sol.busyStudent(startTime, endTime, queryTime));
+    }
+}
+
+class Solution1450 {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    int count=0;
+    for(int i=0;i<startTime.length;i++)
+     {
+    if(startTime[i]<=queryTime && endTime[i]>=queryTime)
+      count++;
+    }
+    return count;
+}
+}
